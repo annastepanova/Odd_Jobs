80850697066106de86220116f948b02de8dfbffc
williamkennedy <wkennedy8@yahoo.com>
2019-11-22 17:19:39 -0500

    fix Nav and add css file for it.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import '../NavBar.css'
 
 export default function NavBar() {
   return (
   <nav className="navbar">
-    <ul>
+    <NavLink exact to="/"><img className="logo" src="OddJobslogo.png" alt="logo" /></NavLink>
+    <ul className="nav-list">
       <li><NavLink exact to="/services">Services</NavLink></li>
       <li><NavLink exact to="/services/support">Support</NavLink></li>
+      <li><NavLink exact to="/contractor_register">Be a Contractor</NavLink></li>
       <li><NavLink exact to="/register">Login/Register</NavLink></li>
+      <div className="nav-btn-list">
+      <button className="translate-button">EN/ES</button>
+      <button className="hire-button">HIRE</button>
+      </div>
     </ul>
   </nav>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,18 +5,7 @@ import '../Home.css'
 export default function Home() {
   return (
     <>
-    <div className="home-logo">
-      <img src="OddJobslogo.png" alt="" />
-    </div>
-    <div className="home-div1">
-      <div className="home-text">
-        <h2>The spot to connect customers with contractors.</h2>
-      </div>
-      <div className="home-search">
-        <input className="home-searchbar"type="text" placeholder="What is your Odd Job?                                          | 33127"/>
-        <button className="home-searchbutton"><img src="./search-icon.svg" alt=""/></button>
-      </div>
-    </div>
+      <h1>Home</h1>
     </>
   )
 }
