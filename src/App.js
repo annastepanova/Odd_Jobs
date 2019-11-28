7e69003f0ac4ebef12bc5197788ed7edf9f37699
Maryali <maryalir@gmail.com>
2019-11-27 21:30:13 -0500

    adding payment page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import ContractorProfile from "./pages/ContractorProfile"
 import "./App.css";
 import Calendar from "./pages/Calendar";
 import { AuthContext } from './context/AuthContext';
+import Payment from "./pages/Payment"
 
 class App extends Component {
   static contextType = AuthContext;
@@ -67,6 +68,7 @@ class App extends Component {
             <Route exact path="/login" component={Login} />
             <Route exact path="/results" component={SearchResults} />
             <Route exact path="/calendar" component={Calendar} />
+            <Route exact path="/payment" component={Payment}/>
             <Route
               exact
               path="/category/:id"
--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -2,7 +2,6 @@
 // import dayGridPlugin from "@fullcalendar/daygrid";
 import React from "react";
 import "../Home.css";
-import { Link } from "react-router-dom";
 
 const Calendar = () =>
   <div>
