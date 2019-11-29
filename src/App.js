5a26ff6a0879552229bcbd6a9abfd13e3ccac4bb
Rob Terrell <Rterrell25@gmail.com>
2019-11-29 16:05:33 -0500

    support route

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
 
   fetchCategories = async () => {
     const { data } = await axios.get("http://localhost:3000/job_categories", {
-    headers: { Authorization: this.context.token }
+      headers: { Authorization: this.context.token }
     });
     this.setState({ categories: data, fetched: true });
   };
@@ -60,16 +60,16 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route
-              
+
               path="/services"
               component={() => <Services categories={categories} />}
             />
-            <Route exact path="/services/support" component={Support} />
+            <Route exact path="/support" component={Support} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/results" component={SearchResults} />
             <Route exact path="/calendar" component={Calendar} />
-            <Route exact path="/payment" component={Payment}/>
+            <Route exact path="/payment" component={Payment} />
             <Route exact path="/about" component={About} />
             <Route
               exact
